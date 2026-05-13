26afc6cffd3a32b6a0935a1b13b8a306881d2d74
Mohamed Shendy <dev.mohamed.shendy@gmail.com>
2026-05-13 19:17:41 +0300

    Initial Commit

---
+++ SwiftSecureNetworking/SwiftSecureNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSecureNetworkingApp.swift
+//  SwiftSecureNetworking
+//
+//  Created by Mohamed Shendy  on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSecureNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
